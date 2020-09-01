477601bc962d9fa8dc91636ede7afd1dcca50fe0
muitnep <muitnep@gmail.com>
2020-09-01 07:51:27 -0300

    Criado o diretorio Funcionarios para melhor organizacao dos arquivos do projeto

---
+++ Funcionarios/Funcionario.js
@@ -0,0 +1,24 @@
+export class Funcionario {
+
+    constructor(nome, salario, cpf) {
+        this._nome = nome;
+        this._salario = salario;
+        this._cpf = cpf;
+    
+        this._bonificacao = 1;
+        this._senha;
+    }
+
+    autenticar(senha) {
+        return senha == this._senha;
+    }
+
+    CadastrarSenha(senha) {
+        this._senha = senha;
+}
+
+}
+
+
+
+
